8ad1d93b41981f695970118bd62b4d060c007879
Shashank Hebbar <shashankhebbar49@gmail.com>
2025-02-10 11:53:58 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+templates = '/templates'
+
+# Homepage
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+# About Me
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Projects
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+# Articles
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+# Contact Me
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
